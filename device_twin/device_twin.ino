5dc4cbc559de5ff3afd9d49b2396647ff9252733
Roy Sprowl <roys@jetstreamsoftware.com>
2018-01-05 14:14:03 -0800

    Update sample to work with ESP8266 v2.4

--- device_twin/device_twin.ino
+++ device_twin/device_twin.ino
@@ -3,11 +3,9 @@
 
 // Please use an Arduino IDE 1.6.8 or greater
 
+// You must set the device id, device key, IoT Hub name and IotHub suffix in
+// iot_configs.h
 #include "iot_configs.h"
-
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <WiFiUdp.h>
 
 #include <AzureIoTHub.h>
 #if defined(IOT_CONFIG_MQTT)
@@ -17,14 +15,13 @@
 #endif
 
 #include "sample.h"
+#include "esp8266/sample_init.h"
 
 static char ssid[] = IOT_CONFIG_WIFI_SSID;
 static char pass[] = IOT_CONFIG_WIFI_PASSWORD;
 
 void setup() {
-    initSerial();
-    initWifi();
-    initTime();
+    sample_init(ssid, pass);
 }
 
 // Azure IoT samples contain their own loops, so only run them once
@@ -42,44 +39,4 @@ void loop() {
     {
       delay(500);
     }
-}
-
-void initSerial() {
-    // Start serial and initialize stdout
-    Serial.begin(115200);
-    Serial.setDebugOutput(true);
-}
-
-void initWifi() {
-    // Attempt to connect to Wifi network:
-    Serial.print("\r\n\r\nAttempting to connect to SSID: ");
-    Serial.println(ssid);
-    
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    
-    Serial.println("\r\nConnected to wifi");
-}
-
-void initTime() {  
-   time_t epochTime;
-
-   configTime(0, 0, "pool.ntp.org", "time.nist.gov");
-
-   while (true) {
-       epochTime = time(NULL);
-
-       if (epochTime == 0) {
-           Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
-           delay(2000);
-       } else {
-           Serial.print("Fetched NTP epoch time is: ");
-           Serial.println(epochTime);
-           break;
-       }
-   }
 }
